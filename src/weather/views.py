14b2dcd101faa34b394d483c9f7362f7a3e7ed28
luta-wolf <denis.skrynnikov.job@gmail.com>
2022-12-08 02:34:35 +0300

    adding api + requirements.txt

--- src/weather/views.py
+++ src/weather/views.py
@@ -1,6 +1,15 @@
+import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
+	appid = '30814faf0bf2eaa9e852c19ea73a6522'
+	url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid=' + appid
+
+	city = 'London'
+	res = requests.get(url.format(city))
+
+	print(res.text)
+
 	return render(request, 'weather/index.html')
